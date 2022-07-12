8329e38ebc4c4652c7b7d1ef906f4248c776a9ea
Programador1RMV <programador1@pandoradigital.co>
2022-07-12 11:43:05 -0500

    ajuste antes de realease dividir

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function sumar() {
   document.getElementById("resultado").value = primer_numero + segundo_numero;
 }
 
-function restar(){
+function dividir(){
   let primer_numero = parseInt(document.getElementById("num1").value);
   let segundo_numero = parseInt(document.getElementById("num2").value);
 
-  document.getElementById("resultado").value = primer_numero - segundo_numero;
+  document.getElementById("resultado").value = primer_numero / segundo_numero;
 }
