3a3b922ea6b2b27e6f055a1ce568627e832879b2
Programador1RMV <programador1@pandoradigital.co>
2022-07-12 11:34:38 -0500

    funcion de sumar

---
+++ index.js
@@ -0,0 +1,6 @@
+function sumar() {
+  let primer_numero = parseInt(document.getElementById("num1").value);
+  let segundo_numero = parseInt(document.getElementById("num2").value);
+
+  document.getElementById("resultado").value = primer_numero + segundo_numero;
+}
