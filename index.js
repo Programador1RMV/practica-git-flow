a5c4de56fb9e72231cf2397d96c033de9928ea01
Programador1RMV <programador1@pandoradigital.co>
2022-07-12 11:38:44 -0500

    funcion de restar

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function sumar() {
 
   document.getElementById("resultado").value = primer_numero + segundo_numero;
 }
+
+function restar(){
+  let primer_numero = parseInt(document.getElementById("num1").value);
+  let segundo_numero = parseInt(document.getElementById("num2").value);
+
+  document.getElementById("resultado").value = primer_numero - segundo_numero;
+}
